49f60c56a3832e60e33ca69d4ea65f8e2db3bf42
Iman_IDG <i.zamaninasab@gmail.com>
2024-11-06 14:45:08 -0800

    add_all_sensors

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,36 @@ void printSerialNumber();
 bool GetSerialData();
 unsigned int getCS(byte len);
 //________________________________setup loop, runs once______________________________
+
+
+// if using a CO2Meter CM-200 Sensor Development Board, set this to 1 if you want the LEDs to flash
+// if you're wiring directly to the sensor, set this to 0
+#define CM200 0
+
+#if CM200
+#define GREEN_LED 4
+#define BLUE_LED 5
+#define RED_LED 6
+
+uint8_t ind = GREEN_LED;
+#endif
+
+// if you want the sensor to stream set this to 1
+// if you want to use the sensor in polling mode, set this to 0 and set the delay_ms to desired delay time
+#define STREAMING 1
+const int delay_ms = 500;
+
+//int co2 = 0;
+double multiplier = 1; // 1 for 2% =20000 PPM, 10 for 20% = 200,000 PPM
+
+const uint8_t buff_size = 64;
+//uint8_t buff[buff_size];
+uint8_t indexCoz = 0;
+
+int fill_buffer();  // function prototypes here
+int format_output();
+double get_co2(char * str);
+
 void setup()
 {
   digitalWrite(sleepPin, HIGH); //puts unit active (HIGH), sleep (LOW)
@@ -68,6 +98,31 @@ void setup()
   /*****************************  MQ CAlibration ********************************************/ 
   MQ6.serialDebug(true);
   sampleTime = millis();
+
+  
+  Serial.print("\n\n");
+  Serial.println("             AN128 Ardunio to Cozir CO2 Sensor - Demonstration code 11/29/2017\n\n");
+
+#if CM200
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+#endif
+  
+  Serial2.begin(9600); // Start serial communications with sensor
+
+  Serial2.println("M 6"); // send Mode for Z and z outputs
+  // "Z xxxxx z xxxxx" (CO2 filtered and unfiltered)
+
+#if STREAMING
+  Serial2.println("K 1");  // set streaming mode
+#else
+  Serial2.println("K 2");  // set streaming mode
+#endif
 }
 
 
@@ -78,23 +133,94 @@ void loop()
   if (millis() >= (sampleRate + sampleTime))
   {
     sampleTime = millis();  //resets timer before printing output
-    //sendRequest(readPM, 5, 32); //comms request to sensor, command string, string length ex CRC, response length in bytes inc CRC
-    if (GetSerialData()) {  //request to get data, if CRC OK, then print and display
-      Serial.print(( data[4] & 0x3F ) << 8 | data[5]); Serial.print(", "); //PM1 Standard Smoke
-      Serial.print(( data[6] & 0x3F ) << 8 | data[7]); Serial.print(", "); //PM2.5 Standard Smoke
-      Serial.print(( data[8] & 0x3F ) << 8 | data[9]); Serial.print(", "); //PM10 Standard Smoke
-    } else {
-      Serial.print(F("No data"));
-    }
-    Serial.println();
     
     MQ6.update(); // Update data, the arduino will read the voltage from the analog pin
     MQ6.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
     MQ6.serialDebug(); // Will print the table on the serial port
+  double co2_flt = 0.0;
+  double co2_raw = 0.0;
+  char buff[buff_size];
+
+#if CM200
+  // if using a CM-200 we can cycle through the LEDs to high and low so we can see that we're in the loop
+  if (digitalRead(ind) == HIGH)
+    digitalWrite(ind, LOW);
+  else
+    digitalWrite(ind, HIGH);
+
+  ind++;
+  if (ind > RED_LED)
+    ind = GREEN_LED;
+#endif
+
+  Serial2.flush();
+#if STREAMING == 0
+  Serial2.println("Z");  // set streaming mode
+#endif
+
+  int timeout = 5000;
+  while ( Serial2.available() <= 0 && timeout-- > 0) {
+    Serial.print(".");
+    delay(10);
+  }
+  Serial.println("");
+
+  delay(10); //delay 10ms to give the Serial buffer some time to fill
+  Serial.print("Sensor Response: ");
+  indexCoz = 0;
+  while ( Serial2.available() > 0 ) {
+    char tmp = Serial2.read();
+    buff[indexCoz] = tmp;
+    Serial.print((char)tmp);
+
+    if (tmp == '\r')
+      break;
+
+    indexCoz++;
+    if (indexCoz >= buff_size) //let's not overflow the buffer
+      break;
+  }
+
+  Serial.println("");
+
+  char *p = buff;
+  char *str;
+  
+  while ((str = strtok_r(p, " ", &p)) != NULL) {
+    if (str[0] == 'Z') {
+      str = strtok_r(p, " ", &p);
+      co2_flt = get_co2(str);
+    }
+    if (str[0] == 'z') {
+      str = strtok_r(p, " ", &p);
+      co2_raw = get_co2(str);
+    }
+  }
+
+  Serial.println("");
+  Serial.print("***** Filtered CO2 PPM: ");
+  Serial.print(co2_flt);
+  Serial.print("\r\n");
+#if STREAMING   // we don't need to print this out if we're not streaming
+  Serial.print("***** Raw CO2 PPM: ");
+  Serial.print(co2_raw);
+  Serial.print("\r\n");
+#endif
+
+  delay(delay_ms);
   }
 }
 
 
+double get_co2(char * str) {
+  for (int i = 0; i < strlen(str); i++) { //cycle through char array making sure we're all digits
+    if (!isDigit(str[i])) {
+     str[i] = '\0';
+    }
+  }
+
+  return atof(str) * multiplier; // this is not great we could crash here if the first character is not a number
+}
 //___________________________sub routine to read value from module_________________
 
 
